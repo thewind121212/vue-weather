e3a75f4942c7a6968ab5c6572a94ea4b5e2a7c8f
Linh Tran <52797453+thewind121212@users.noreply.github.com>
2024-12-03 17:55:12 +0700

    Update Layout.vue
    fix the gap more large

--- src/Layout.vue
+++ src/Layout.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="w-svw h-svh padding-main bg-[#000000] dark">
     <div className="flex h-full w-full items-center justify-center">
-      <div className="grid h-full w-full gap-4  p-2 grid-cols-4 grid-rows-2 rounded-lg">
+      <div className="grid h-full w-full gap-8 p-2 grid-cols-4 grid-rows-2 rounded-lg">
 
         <div className="col-span-2 row-span-1 bg-[#1A1A1A] rounded-2xl flex items-center justify-center text-[#FFFF] text-5xl">
           cloudy
